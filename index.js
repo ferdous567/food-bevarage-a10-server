7c4845eb8808faa8059c3e441b3485d3b6470feb
ferdous <papiaferdous43@gmail.com>
2023-10-19 02:00:37 +0600

    cached node_modules

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app =  express();
 const port = process.env.PORT || 5000;
 
@@ -28,6 +28,59 @@ async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
     await client.connect();
+
+    const productCollection = client.db('foodDB').collection('product');
+    const brandCollection = client.db('foodDB').collection('brands');
+    
+
+    app.get('/products', async(req, res) =>{
+      const cursor = productCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    
+    // brands get
+    app.get('/brands', async(req, res) =>{
+      const cursor = brandCollection.find();
+      const result = await cursor.toArray();
+      res.send(result)
+    })
+    
+
+    app.post('/products', async(req, res) =>{
+      const newProduct = req.body;
+      console.log(newProduct);
+      const result = await productCollection.insertOne(newProduct);
+      res.send(result);
+    })
+
+    // brand post
+    app.post('/brands', async(req, res) =>{
+      const newBrand = req.body;
+      console.log(newBrand);
+      const result = await brandCollection.insertOne(newBrand);
+      res.send(result);
+    })
+
+    // single brand
+    app.get('/brands/:id', async (req, res) => {
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const result = await brandCollection.findOne(filter);
+      console.log(result);
+      res.send(result);
+    })
+
+    // Brand wish product get
+     app.get("/brand-product/:id", async (req, res) => {
+        const brandId = req.params.id;
+        const filter = {brand_id : brandId }
+        const result = await productCollection.find(filter).toArray();
+        res.send(result)
+    });
+
+  
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
