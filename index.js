9327fe01d7bd032fb98a9ff3572742f9a239dcd4
ferdous <papiaferdous43@gmail.com>
2023-10-20 18:59:02 +0600

    client.connect() is commented and vercel deploy is completed

--- index.js
+++ index.js
@@ -27,7 +27,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('foodDB').collection('product');
     const brandCollection = client.db('foodDB').collection('brands');
